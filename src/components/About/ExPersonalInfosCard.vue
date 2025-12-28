9442bee4360455fdd1d735de52d426e19912db40
JEEVAN KAMI <jeevankami276@gmail.com>
2025-12-28 21:49:07 +0545

    Updated ABout Page-Card

---
+++ src/components/About/ExPersonalInfosCard.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+  data() {
+    return {
+      cardDetails: [
+        { count: 6, text: "months of experience" },
+        { count: 2, text: "completed projects" },
+        { count: 2, text: "happy customers" },
+        { count: 0, text: "awards won" },
+      ],
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="flex flex-wrap justify-center gap-5">
+    <div
+      v-for="(card, index) in cardDetails"
+      :key="index"
+      class="flex flex-col justify-center items-center w-50 h-30 p-5 border border-neutral-300 dark:border-neutral-700 rounded-xl bg-white dark:bg-neutral-900 shadow-md hover:shadow-lg transition-all duration-300"
+    >
+      <h1 class="text-5xl font-bold text-amber-400">
+        {{ card.count }}<sup>+</sup>
+      </h1>
+      <hr class="w-10 border-neutral-300 dark:border-neutral-700 my-2" />
+      <h2
+        class="uppercase text-center font-semibold text-gray-700 dark:text-neutral-400"
+      >
+        {{ card.text }}
+      </h2>
+    </div>
+  </div>
+</template>
---
+++ src/components/About/PersonalInfosCard.vue
@@ -0,0 +1,40 @@
+<script>
+import { Gauge, Layout, Puzzle, Hammer, Component } from "lucide-vue-next";
+
+export default {
+  components: {
+    Layout,
+    Gauge,
+    Puzzle,
+    Hammer,
+  },
+  data() {
+    return {
+      cardDetails: [
+        { text: "Architect", icon: "Layout" },
+        { text: "Optimizer", icon: "Gauge" },
+        { text: "Problem-Solver", icon: "Puzzle" },
+        { text: "Builder", icon: "Hammer" },
+      ],
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="flex flex-wrap justify-center gap-5">
+    <div
+      v-for="(card, index) in cardDetails"
+      :key="index"
+      class="flex flex-col justify-center items-center w-50 h-30 p-5 border border-neutral-300 dark:border-neutral-700 rounded-xl bg-white dark:bg-neutral-900 shadow-md hover:shadow-lg transition-all duration-300"
+    >
+      <Component :is="card.icon" class="w-8 h-8 text-yellow-400" />
+      <hr class="w-10 border-neutral-300 dark:border-neutral-700 my-2" />
+      <h2
+        class="uppercase text-center font-semibold text-gray-700 dark:text-neutral-400"
+      >
+        {{ card.text }}
+      </h2>
+    </div>
+  </div>
+</template>
